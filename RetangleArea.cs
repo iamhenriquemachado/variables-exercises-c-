140f63f6a5cdab54a49687af58c2efeea8e7eb53
Henrique de Castro Machado <heyhenriquecastro@gmail.com>
2024-07-12 23:03:29 -0300

    Create RetangleArea.cs

---
+++ RetangleArea.cs
@@ -0,0 +1,17 @@
+using System;
+
+
+namespace retangle_area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double height = 55.89;
+            double width = 77.01;
+
+            Console.WriteLine("Retangle Area: " + height * width);
+        }
+
+    }
+}
