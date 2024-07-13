866dd1c8e20f0f7b2f5b54c9ae1d849ac17f5e48
Henrique de Castro Machado <heyhenriquecastro@gmail.com>
2024-07-12 22:59:36 -0300

    Create MathOperations.cs

---
+++ MathOperations.cs
@@ -0,0 +1,30 @@
+using System;
+
+
+namespace MathOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = 3;
+            int n2 = 7;
+            int n3 = 6;
+
+            // Adição
+            Console.WriteLine(n3 + n2);
+
+            // Subtração
+            Console.WriteLine(n2 - n1);
+
+            // Multiplicação
+            Console.WriteLine(n3 * n2);
+
+            // Divisão
+            Console.WriteLine(n2 / n1);
+
+
+        }
+
+    }
+}
